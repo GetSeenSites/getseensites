0227a097db7e2490ba55704440d94b308e91336a
GetSeenSites <contactgetseensites@gmail.com>
2025-06-11 15:54:31 -0400

    GetStarted.tsx

--- src/pages/GetStarted.tsx
+++ src/pages/GetStarted.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 import ClientIntakeForm from '../components/ClientIntakeForm';
 
 const GetStarted = () => {
-  return <ClientIntakeForm />;
+  const handleSubmit = (formData: any) => {
+    console.log('Form submitted:', formData);
+  };
+
+  return <ClientIntakeForm onSubmit={handleSubmit} />;
 };
 
 export default GetStarted;
