8a8bce1ba44091d703fd17443f6f5bbadda94e89
GetSeenSites <contactgetseensites@gmail.com>
2025-08-10 18:40:38 -0400

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,31 +3,34 @@ import React from 'react';
 import HeroSection from '../components/HeroSection';
 import ProcessCarousel from '../components/ProcessCarousel';
 import WhyUsSection from '../components/WhyUsSection';
-import ServicesSection from '../components/ServicesSection';
+import ConsultingServicesSection from '../components/ConsultingServicesSection';
 import PortfolioSection from '../components/PortfolioSection';
 import TestimonialsSection from '../components/TestimonialsSection';
 import ContactSection from '../components/ContactSection';
 import FAQ from '../components/FAQ';
 import Footer from '../components/Footer';
 
+// Comment out original pricing-based components for potential rollback
+// import ServicesSection from '../components/ServicesSection';
+// import PricingSection from '../components/PricingSection';
+
 const Index = () => {
   return (
     <div className="min-h-screen">
       <HeroSection />
+
+
       <WhyUsSection />
       <ProcessCarousel />
-      <div id="services">
-        <ServicesSection />
-      </div>
-      <div id="work">
-        <PortfolioSection />
-      </div>
       <TestimonialsSection />
       <div id="contact">
         <ContactSection />
       </div>
       <FAQ />
       <Footer />
+      
+      {/* Commented out for potential rollback to pricing model */}
+      {/* <ServicesSection /> */}
     </div>
   );
 };
