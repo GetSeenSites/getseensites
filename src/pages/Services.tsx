cbbb9369e94ca34021d98d6fc396b96c485cc4cd
GetSeenSites <contactgetseensites@gmail.com>
2025-08-10 19:14:21 -0400

    Update Services.tsx

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,1 +1,22 @@
+import React from 'react';
+import ConsultingServicesSection from '../components/ConsultingServicesSection';
+import ContactSection from '../components/ContactSection';
+import FAQ from '../components/FAQ';
+import Footer from '../components/Footer';
 
+const ServicesPage = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="pt-20">
+        <ConsultingServicesSection />
+      </div>
+      <div id="contact">
+        <ContactSection />
+      </div>
+      <FAQ />
+      <Footer />
+    </div>
+  );
+};
+
+export default ServicesPage;
